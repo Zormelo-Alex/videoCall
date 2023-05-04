42d2c5b772fd088dcbeb67ed9d9c8aaace08a118
Zormelo_Alex <alexzormelo9@gmail.com>
2023-05-04 00:20:01 +0000

    ...

---
+++ script.js
@@ -0,0 +1,132 @@
+const localVideo = document.getElementById('local-video');
+const remoteVideo = document.getElementById('remote-video');
+const startButton = document.getElementById('start-call');
+const endButton = document.getElementById('end-call');
+ 
+let localStream;
+let remoteStream;
+let peerConnection;
+let socket;
+ 
+startButton.onclick = startCall;
+endButton.onclick = endCall;
+ 
+function startCall() {
+  console.log('Starting call...');
+ 
+  const serverUrl = 'ws://' + window.location.hostname + ':8080';
+  const socket = new WebSocket(serverUrl);
+ 
+  socket.addEventListener('open', function(event) {
+    console.log('WebSocket connection opened:', event);
+ 
+    const offer = peerConnection.createOffer();
+ 
+    peerConnection.setLocalDescription(offer).then(function() {
+      const message = {
+        type: 'offer',
+        payload: offer,
+      };
+ 
+      console.log('Sending offer:', message);
+ 
+      socket.send(JSON.stringify(message));
+    });
+  });
+ 
+  socket.addEventListener('message', function(event) {
+    console.log('Received message from server:', event);
+ 
+    const message = JSON.parse(event.data);
+ 
+    switch (message.type) {
+      case 'answer':
+        handleAnswerMessage(message.payload);
+        break;
+      case 'icecandidate':
+        handleIceCandidateMessage(message.payload);
+        break;
+      default:
+        console.error('Unknown message type:', message.type);
+        break;
+    }
+  });
+ 
+  socket.addEventListener('close', function(event) {
+    console.log('WebSocket connection closed:', event);
+  });
+}
+ 
+function endCall() {
+  localStream.getTracks().forEach(track => track.stop());
+  remoteStream.getTracks().forEach(track => track.stop());
+  localVideo.srcObject = null;
+  remoteVideo.srcObject = null;
+  peerConnection.close();
+  peerConnection = null;
+}
+ 
+async function handleConnection(event, socket) {
+  if (event.candidate) {
+    const message = JSON.stringify({
+      type: 'icecandidate',
+      data: event.candidate,
+    });
+    socket.send(message);
+  }
+}
+
+ 
+function handleTrack(event) {
+  remoteStream = event.streams[0];
+  remoteVideo.srcObject = remoteStream;
+}
+
+async function handleOffer(offer) {
+  try {
+    const configuration = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] };
+    peerConnection = new RTCPeerConnection(configuration);
+ 
+    peerConnection.addEventListener('icecandidate', handleConnection);
+    peerConnection.addEventListener('track', handleTrack);
+ 
+    localStream.getTracks().forEach(track => peerConnection.addTrack(track, localStream));
+ 
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(offer));
+ 
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+ 
+    const message = {
+      type: 'answer',
+      data: answer,
+    };
+ 
+    ws.send(JSON.stringify(message));
+  } catch (error) {
+    console.error('Error handling offer:', error);
+  }
+}
+ 
+function handleAnswerMessage(answer) {
+  console.log('Received answer:', answer);
+ 
+  const remoteDescription = new RTCSessionDescription(answer);
+ 
+  peerConnection.setRemoteDescription(remoteDescription).then(function() {
+    console.log('Remote description set successfully');
+  });
+}
+ 
+function handleIceCandidate(event) {
+  console.log('Received ICE candidate:', event);
+ 
+  const message = {
+    type: 'icecandidate',
+    payload: event.candidate,
+  };
+ 
+  console.log('Sending ICE candidate:', message);
+ 
+  socket.send(JSON.stringify(message));
+}
---
+++ server.js
@@ -0,0 +1,67 @@
+const WebSocket = require('ws');
+const httpServer = require('http').createServer();
+const io = require('socket.io')(httpServer, {
+  cors: {
+    origin: '*',
+  },
+});
+ 
+const wss = new WebSocket.Server({ port: 8080 });
+ 
+wss.on('connection', function connection(ws) {
+  console.log('New client connected');
+ 
+  ws.on('message', function incoming(message) {
+    console.log('Received message:', message);
+ 
+    // Broadcast the message to all other clients
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+  });
+
+  ws.on('close', function close() {
+    console.log('Client disconnected');
+  });
+});
+
+io.on('connection', function(socket) {
+  console.log('Client connected:', socket.id);
+ 
+  socket.on('message', function(message) {
+    console.log('Received message from', socket.id, ':', message);
+ 
+    const parsedMessage = JSON.parse(message);
+ 
+    switch (parsedMessage.type) {
+      case 'offer':
+      case 'answer':
+      case 'icecandidate':
+        broadcastMessage(socket.id, parsedMessage);
+        break;
+      default:
+        console.error('Unknown message type:', parsedMessage.type);
+        break;
+    }
+  });
+ 
+  socket.on('disconnect', function() {
+    console.log('Client disconnected:', socket.id);
+  });
+});
+ 
+function broadcastMessage(senderId, message) {
+  io.sockets.sockets.forEach(function(socket) {
+    if (socket.id !== senderId) {
+      socket.send(JSON.stringify(message));
+    }
+  });
+}
+
+const PORT = process.env.PORT || 8080;
+
+httpServer.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
